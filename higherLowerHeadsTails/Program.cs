7872daee9000ecb7352c1575e7cd4ae9bfd53801
Marcus Moore <marcusmoorejr1230@gmail.com>
2023-10-23 14:35:56 -0500

    Added the headstails portion of the console app, and accounted for possible error when inputting an integer higher or lower than inteded

--- higherLowerHeadsTails/Program.cs
+++ higherLowerHeadsTails/Program.cs
@@ -7,7 +7,7 @@ namespace higherLowerHeadsTails
 
         static void Main(string[] args)
         {
-            lowHigh();
+            headTail();
         }
 
 
@@ -24,7 +24,7 @@ namespace higherLowerHeadsTails
             {
                 string temp1 = Console.ReadLine();
                 int guess;
-                if (int.TryParse(temp1, out guess))
+                if (int.TryParse(temp1, out guess) && (Convert.ToInt32(temp1) <= 50) && (Convert.ToInt32(temp1) >= 1))
                 {
                     guess = Convert.ToInt32(temp1);
 
@@ -56,6 +56,43 @@ namespace higherLowerHeadsTails
 
         public static void headTail()
         {
+            Random rnd = new Random();
+            int value = rnd.Next(1, 3);
+            bool keepGoing = true;
+
+            Console.WriteLine("Heads or Tails? Press 0 to quit.\n 1. Heads\n 2. Tails");
+            while (keepGoing) {
+                string temp1 = Console.ReadLine();
+                int guess;
+           
+            
+                if (int.TryParse(temp1, out guess) && (Convert.ToInt32(temp1) <= 2) && (Convert.ToInt32(temp1) >= 1))
+                {
+                    guess = Convert.ToInt32(temp1);
+                    if (guess == value)
+                    {
+                        keepGoing = false;
+                        Console.WriteLine("You got it!");
+                    }
+                    else if(guess != value)
+                    {
+                        keepGoing= false;
+                        Console.WriteLine("That's Wrong!");
+                    }
+                    else if (guess == 0)
+                    {
+                        keepGoing = false;
+                        Console.WriteLine("Thanks for playing!");
+                    }
+                    
+                }
+                else
+                {
+                    Console.WriteLine("That is not a valid Answer");
+                }
+
+            }
+
 
         }
     }
